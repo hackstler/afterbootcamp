4d6825ee5f3db2588e241b2045e01b4bcb167f94
hackstler <sergio@hackstler.com>
2024-06-19 23:53:41 +0200

    add meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,6 +45,24 @@ export default function RootLayout({
       <head>
         <meta charSet='UTF-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta
+          name='keywords'
+          content='bootcamp, career growth, software development, devops'
+        />
+        <meta name='author' content='After Bootcamp' />
+        <meta property='og:title' content='After Bootcamp' />
+        <meta
+          property='og:description'
+          content='Grow your career with After Bootcamp'
+        />
+        <meta property='og:image' content='/path/to/image.jpg' />
+        <meta property='og:url' content='https://afterbootcamp.es' />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:title' content='After Bootcamp' />
+        <meta
+          name='twitter:description'
+          content='Grow your career with After Bootcamp'
+        />
       </head>
       <body className={montserrat.className}>
         {children}
