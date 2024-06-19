52c5deb5a235e726e790eee5f2de9c5b93f1d28f
hackstler <sergio@hackstler.com>
2024-06-19 23:06:43 +0200

    try add seo improvements

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Montserrat } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,6 +18,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <Head>
+        <meta charSet='UTF-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta
+          name='keywords'
+          content='bootcamp, career growth, software development, devops'
+        />
+        <meta name='author' content='After Bootcamp' />
+        <meta property='og:title' content='After Bootcamp' />
+        <meta
+          property='og:description'
+          content='Grow your career with After Bootcamp'
+        />
+        <meta property='og:image' content='/path/to/image.jpg' />
+        <meta property='og:url' content='https://www.afterbootcamp.com' />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:title' content='After Bootcamp' />
+        <meta
+          name='twitter:description'
+          content='Grow your career with After Bootcamp'
+        />
+        {/* <meta name='twitter:image' content='/path/to/image.jpg' /> */}
+      </Head>
       <body className={montserrat.className}>{children}</body>
     </html>
   );
---
+++ next-stiemap.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.afterbootcamp.es',
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+  };
