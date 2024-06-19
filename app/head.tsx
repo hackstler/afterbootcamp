a95bb18ed6edbb5dd116774cf1ceb03f16a8452d
hackstler <sergio@hackstler.com>
2024-06-19 23:35:23 +0200

    deploy risky tag manager

---
+++ app/head.tsx
@@ -0,0 +1,27 @@
+export default function Head() {
+  return (
+    <>
+      <title>After Bootcamp</title>
+      <meta charSet='UTF-8' />
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <meta
+        name='keywords'
+        content='bootcamp, career growth, software development, devops, after bootcamp, afterbootcamp'
+      />
+      <meta name='author' content='After Bootcamp' />
+      <meta property='og:title' content='After Bootcamp' />
+      <meta
+        property='og:description'
+        content='Grow your career with After Bootcamp'
+      />
+      {/* <meta property='og:image' content='/path/to/image.jpg' /> */}
+      <meta property='og:url' content='https://afterbootcamp.es' />
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:title' content='After Bootcamp' />
+      <meta
+        name='twitter:description'
+        content='Grow your career with After Bootcamp'
+      />
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,47 +1,74 @@
-import type { Metadata } from "next";
+"use client";
+
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 import { Inter, Montserrat } from "next/font/google";
-import Head from "next/head";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 const montserrat = Montserrat({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "After Bootcamp",
-  description: "Grow your career with After Bootcamp",
-};
+const GA_TRACKING_ID = "G-LMZ1RWB8J4"; // Reemplaza con tu ID de seguimiento
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      window.gtag("config", GA_TRACKING_ID, {
+        page_path: url,
+      });
+    };
+
+    handleRouteChange(pathname); // Inicializar para la primera carga
+
+    window.addEventListener("popstate", () =>
+      handleRouteChange(window.location.pathname)
+    );
+
+    return () => {
+      window.removeEventListener("popstate", () =>
+        handleRouteChange(window.location.pathname)
+      );
+    };
+  }, [pathname]);
+
   return (
-    <html lang='en'>
-      <Head>
+    <html lang='es'>
+      <head>
         <meta charSet='UTF-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <meta
-          name='keywords'
-          content='bootcamp, career growth, software development, devops'
+        <link
+          rel='stylesheet'
+          href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
         />
-        <meta name='author' content='After Bootcamp' />
-        <meta property='og:title' content='After Bootcamp' />
-        <meta
-          property='og:description'
-          content='Grow your career with After Bootcamp'
+      </head>
+      <body className={montserrat.className}>
+        {children}
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
         />
-        <meta property='og:image' content='/path/to/image.jpg' />
-        <meta property='og:url' content='https://www.afterbootcamp.com' />
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta name='twitter:title' content='After Bootcamp' />
-        <meta
-          name='twitter:description'
-          content='Grow your career with After Bootcamp'
+        <Script
+          id='gtag-init'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
         />
-        {/* <meta name='twitter:image' content='/path/to/image.jpg' /> */}
-      </Head>
-      <body className={montserrat.className}>{children}</body>
+      </body>
     </html>
   );
 }
