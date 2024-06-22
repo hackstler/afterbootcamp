0913145c779ddd267961e63b3bb3a4b9da7421a1
hackstler <sergio@hackstler.com>
2024-06-22 20:29:17 +0200

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,38 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 const montserrat = Montserrat({ subsets: ["latin"] });
 
+export const metadata: Metadata = {
+  title: "Afterbootcamp",
+  description: "Grow your career with After Bootcamp",
+  keywords: [
+    "bootcamp",
+    "career growth",
+    "software development",
+    "devops",
+    "after bootcamp",
+    "afterbootcamp",
+    "afterbootcamp.es",
+    "seniority",
+    "developer",
+    "software engineer",
+    "software architect",
+    "tech lead",
+    "cto",
+    "career path",
+    "career development",
+    "career growth",
+  ],
+  openGraph: {
+    title: "Afterbootcamp",
+    description: "Grow your career with After Bootcamp",
+    url: "https://afterbootcamp.es",
+    type: "website",
+    images: [
+      "https://storage.googleapis.com/afterbootcamp/openart-image_Zssjbb-I_1716376231201_raw.png",
+    ],
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
